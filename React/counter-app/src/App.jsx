0242fd31819042f1860f90319e22cf34beeeb7ff
HariMenon2002 <ultronhari7@gmail.com>
2024-06-25 19:02:25 +0530

    todo app

--- React/counter-app/src/App.jsx
+++ React/counter-app/src/App.jsx
@@ -1,37 +1,65 @@
 import { useState } from "react";
-//const a=10;
-// let state={        React will not care about this, as it doesnt consider this as a state variable
-//   count:0          //this is the reason why we use useState hook
-// }
 
+//todo application using react
+// {
+//   todos:[{title:"todo1",description:"First todo",completed:false}]
+// }      
+//the above should be my state , here my state is in form of an array
 function App() {
-  const [count,setCount]=useState(0);  //0 will be initial value of count
+  const [todos,setTodos]=useState([{  //another thing to note is that i declare state variables inside functions as well
+    title:"Go to gym",
+    description:"Go to gym from 7-8",
+    completed:false
+    },{
+    title:"Study DSA",
+    description:"Study dsa from 9-10",
+    completed:true
+  }]);  
+
+  function addTodo() {
+    setTodos([...todos,{          //...todos spreads all value of todos
+      title:"new todo",
+      description:"desc of new todo"
+    }])
+  }
 
   return (
     <div>
-      <CustomButton count={count} setCount={setCount}></CustomButton>  
-      <CustomButton count={count+100} setCount={setCount}></CustomButton>  
-      {/* The count and setCount will be the parameters that go to the props */}
+      {/* doing only {JSON.stringify(todos)} will work but it is very unclean */}
+      {/* <Todo title="harkirat" description="harkirat2"></Todo> */}
+      {/* <Todo title={todos[0].title} description={todos[0].description}></Todo>
+      <Todo title={todos[1].title} description={todos[1].description}></Todo> */}
+      {/* //it is better to use a for loop */}
+
+      <button style={{border:10}} onClick={addTodo}>Add a remote todo</button> 
+
+      {todos.map(function(todo){
+        return <Todo title={todo.title} description={todo.description}></Todo>
+      })}
+
+      <DummyButton></DummyButton>
     </div>
   )
 }
 
 
-function CustomButton(props){  //another component
-//it is better to make these components externally instead of putting all of them in App()
-    function onClickHandler(){
-      props.setCount(props.count+1);
-    }
-    return <button onClick={onClickHandler}>
-      Counter {props.count}
-    </button>
+function Todo(props){ 
+
+    return <div>
+      <h1>{props.title}</h1>
+      <h1>{props.description}</h1>
+      
+    </div>
 }
 
+function DummyButton(){  //this is just to show that if parent is re-rendered, its children also will be re-rendered even if it doesnt use state variable
+  console.log("re rendered dummy button");
+  return <button>asdad</button>
+}
 export default App
 
-//to get javascript variable inside a react component , you should use {}
+// note:
+// instead of <Todo title="harkirat" description="harkirat2"></Todo>  i can also do
+// <Todo title="harkirat" description="harkirat2" />
 
-//this is xml used here and not html so syntax can vary, for eg you cant use
-// <button onclick="hari()"> 
-
-
+//if todos=[1,2] , [...todos,3] is actually [1,2,3]
