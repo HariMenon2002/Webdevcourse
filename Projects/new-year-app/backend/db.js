419664792a3a0db407748282cc5de1042f0165f8
HariMenon2002 <ultronhari7@gmail.com>
2024-06-26 14:28:34 +0530

    database set

---
+++ Projects/new-year-app/backend/db.js
@@ -0,0 +1,23 @@
+/*Schema looks something like
+ Tod{
+    title:string,
+    description:string,
+    completed:boolean
+ }
+*/
+
+const mongoose=require("mongoose");
+//mongodb+srv://b20ec33:l0NVOiTPTWAncRHw@cluster0.itjmjiy.mongodb.net/todos
+
+mongoose.connect("mongodb+srv://b20ec33:l0NVOiTPTWAncRHw@cluster0.itjmjiy.mongodb.net/todos");
+const todoSchema=mongoose.Schema({
+    title:String,
+    description:String,
+    completed:Boolean
+})
+
+const todo=mongoose.model('todos',todoSchema);
+module.exports={
+    todo:todo            //even if you write just todo also it is fine as key and value are same
+}
+
