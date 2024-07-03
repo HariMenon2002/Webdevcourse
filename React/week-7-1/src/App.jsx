904428cd7b8395330ab3781b69621630056e3b7a
HariMenon2002 <ultronhari7@gmail.com>
2024-07-04 01:31:20 +0530

    selector

--- React/week-7-1/src/App.jsx
+++ React/week-7-1/src/App.jsx
@@ -1,20 +1,13 @@
-//State management using recoil
+//Selector
 
-//whole project can be divided into states and components
-//State management is a cleaner way to store state of your app
-//Recoil is a state management library for react
-
-//This concept can reduce the re renders which were prominent in context api
-
-//no context logic and state variable should be here
-
+//It is like useMemo , ie it can be used when something is derived from a bunch of dependencies, for eg here const isEven depends on the variable val
 import { RecoilRoot, useRecoilState ,useRecoilValue, useSetRecoilState} from "recoil";
 import { countAtom } from "./store/atoms/count";
+import { useMemo } from "react";
 
 function App(){
   
-  //here we have to wrap it with RecoilRoot
-  //wherever you have to use atom logic ,we have to wrap it with recoil root
+ 
 
   return <div>
     <RecoilRoot>
@@ -24,7 +17,7 @@ function App(){
 }
 
 function Count(){
-  //it is also okay to put return <div> <RecoilRoot>... </RecoilRoot>here as Count() doesnt use any recoil logic
+  
   return <div>
     <CountRenderer/>
     <Buttons />
@@ -32,32 +25,26 @@ function Count(){
 }
 
 function CountRenderer(){
-  //here i just need the value and need not update so i can use 
+  
   const count=useRecoilValue(countAtom);
 
-  //if i need both i will just do
-  //const [count,setCount]=useRecoilState(countAtom)
   return <div>
     {count}
   </div>
 }
 
 function Buttons(){
-  //here i need both value and update
-  //const [count,setCount]=useRecoilState(countAtom);
-
-  //but actually when you think about it ,you dont need the count value here,you just need the updation .The problem when you do above is that, Buttons() also gets re rendered which is unnecessary
-
+  
   const setCount=useSetRecoilState(countAtom);
   console.log('re-render button')
   return <div>
     <button onClick={()=>{
-      //setCount(count+1)       //this can be used only if i define count here as in the first approach
-      setCount(count=>count+1) //here parameter in setCount should be given like this as it now just symbolises a function and not the actual state variable count
+     
+      setCount(count=>count+1) 
     }}>Increase</button>
 
     <button onClick={()=>{
-      //setCount(count-1)
+      
       setCount(cnt=>cnt-1)  
     }}>Decrease</button>
 
@@ -67,41 +54,17 @@ function Buttons(){
 
 function Check(){
   const val=useRecoilValue(countAtom);
-  // if(val%2==0){
-  //   return <div>
-  //     It is even
-  //   </div>
-  // }
-  // else return <div>
-  //   It is odd
-  // </div>
+  
+
+  const isEven=useMemo(()=>{
+    return val%2==0
+  },[val])
 
   return <div>
-    {(val%2==0)?"It is even":"It is odd"}
+    {isEven?"It is even":"It is odd"}
   </div>
 }
 
 export default App
 
 
-//first do npm install recoil
-
-//In src, create a store folder and in that create another folder called atoms. In that create a count.jsx file
-
-/*
-Recoil has the concept of an atom to store the state. An atom is the smallest unit of state. An atom can be defined outside the component. It can be teleported to any component.
-
-An atom is similar to useState Hook ie it can be used to define a state varible
-An atom which is defined in another folder having state variables  can be given to specific components and the beauty is that only those components will rerender.
-
-Things to learn=
-1.RecoilRoot
-2.atom
-3.useRecoilState- works just like useState,ie it gives value and how to update   variable (setState fn)
-4.useRecoilValue- you can use this if you just want the value
-5.useSetRecoilState- if you only want to update variable
-6.selector
-
-
-Another thing to note is that just because we are using recoil, we should completely abandon useState() logic. Recoil is generally used when I need a global state variable which is used by more components
-*/
--- React/week-7-1/src/store/atoms/count.jsx
+++ React/week-7-1/src/store/atoms/count.jsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const countAtom=atom({
     key:"countAtom",       //unique way to identify the atom
@@ -6,3 +6,11 @@ export const countAtom=atom({
 });
 
 //dont have same keys for two diff atoms
+
+export const evenSelector=selector({   //for app7.jsx
+    key:"evenSelector",
+    get:(props)=>{
+        const count=props.get(countAtom);  //ie. evenSelector depends on countAtom 
+        return count%2;
+    }
+})
