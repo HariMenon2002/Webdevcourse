6823213797b311664fe4f31dbb2fb1cb8695e268
HariMenon2002 <ultronhari7@gmail.com>
2024-07-05 10:42:21 +0530

    todo application incomplete

--- React/week-7-1/src/store/atoms/count.jsx
+++ React/week-7-1/src/store/atoms/count.jsx
@@ -14,3 +14,14 @@ export const evenSelector=selector({   //for app7.jsx
         return count%2;
     }
 })
+
+export const filteredTodos=selector({  //for App8.jsx
+    key:"filteredTodos",
+    get:(props)=>{
+        const todos=props.get(todosAtom);
+        const filter=props.get(filterAtom);
+
+        return todos.filter(x=>x.title.includes(filter)||x.description.includes("filter"))
+    }
+
+})
