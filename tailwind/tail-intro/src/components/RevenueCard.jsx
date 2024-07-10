78e76b10df125f4f18953fc1328de3a88eee6275
HariMenon2002 <ultronhari7@gmail.com>
2024-07-11 01:01:54 +0530

    project incomplete

--- tailwind/tail-intro/src/components/RevenueCard.jsx
+++ tailwind/tail-intro/src/components/RevenueCard.jsx
@@ -1,25 +1,32 @@
 //code that i write here will give a full width card
 //it is upto you to change the size using grid
+import { CheckBadge } from "./Check"
 import { RightArrow } from "./RightArrow"
 export const RevenueCard=({title,orderCount,amount})=>{
-    return <div className="bg-white rounded  shadow-md p-2">  {/*rounded is for border radius, shadow-md is for box shadow */}
+    return <div className="bg-white rounded  shadow-md p-2 hover:bg-blue-500">  {/*rounded is for border radius, shadow-md is for box shadow */}
         <div className="text-gray-700">
-            {title}
-             ?
+            <div className="flex">
+                <div>
+                    {title}
+                </div>
+                <div className="ml-1 flex justify-center flex-col">
+                    <CheckBadge></CheckBadge>
+                </div>
+            </div>
         </div>
 
-        <div className="flex justify-between">
-            <div>
+        <div className="flex justify-between pt-2">
+            <div className="font-semibold text-4xl">
                 Rs.{amount}
             </div>
             
-            {orderCount?<div className="flex">
-                <div className="text-blue-400">
-                    {orderCount} orders 
-                </div>
-                <RightArrow></RightArrow>
-
-
+            {orderCount?<div className="flex cursor-pointer underline font-medium flex-col justify-center">
+                <div className="flex">  {/*this is needed as when we do justify-center the arrow would go down */}
+                    <div className="text-blue-400">
+                        {orderCount} orders 
+                    </div>
+                    <RightArrow></RightArrow>
+                </div>  
 
             </div>:null}
         </div>
--- tailwind/tail-intro/src/components/RightArrow.jsx
+++ tailwind/tail-intro/src/components/RightArrow.jsx
@@ -1,5 +1,7 @@
 export const RightArrow=()=>{
-    return <svg   xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className=" fill-blue-700 size-6">
-    <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-  </svg>
+    return <div>
+      <svg   xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className=" fill-blue-700 size-4">
+      <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+      </svg>
+    </div>
 }
