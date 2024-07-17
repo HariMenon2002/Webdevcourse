4bbff77626f6e36fa9be1c7a7b46f54e5dff1012
HariMenon2002 <ultronhari7@gmail.com>
2024-07-17 22:42:41 +0530

    basic frontend ui only

---
+++ Projects/paytm-app/frontend/src/components/SubHeading.jsx
@@ -0,0 +1,6 @@
+export function SubHeading({label}) {
+    return <div className="text-slate-500 text-md pt-1 px-4 pb-4"> 
+      {label}
+    </div>
+  }
+//text-slate-500 is for grey color, px is horizontal padding
