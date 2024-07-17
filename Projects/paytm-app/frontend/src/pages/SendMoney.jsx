2d470e2375b4071f835931538ee90461698c833b
HariMenon2002 <ultronhari7@gmail.com>
2024-07-17 23:59:26 +0530

    signup and dashboard done, working on sendMoney page

--- Projects/paytm-app/frontend/src/pages/SendMoney.jsx
+++ Projects/paytm-app/frontend/src/pages/SendMoney.jsx
@@ -1,4 +1,10 @@
+import { useSearchParams } from "react-router-dom"
+
 export const SendMoney = () => {
+    const [searchParams]=useSearchParams();
+    const id=searchParams.get("id");         //as id and name are query parameters being sent from dashboard
+    const name=searchParams.get("name");
+
     return <div class="flex justify-center h-screen bg-gray-100">
         <div className="h-full flex flex-col justify-center">
             <div
@@ -10,9 +16,9 @@ export const SendMoney = () => {
                 <div class="p-6">
                 <div class="flex items-center space-x-4">
                     <div class="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
-                    <span class="text-2xl text-white">A</span>
+                    <span class="text-2xl text-white">{name[0].toUpperCase()}</span>
                     </div>
-                    <h3 class="text-2xl font-semibold">Friend's Name</h3>
+                    <h3 class="text-2xl font-semibold">{name}</h3>
                 </div>
                 <div class="space-y-4">
                     <div class="space-y-2">
@@ -38,3 +44,5 @@ export const SendMoney = () => {
       </div>
     </div>
 }
+
+//useSearchParams can be used to take data from query
--- Projects/paytm-app/frontend/src/pages/Signup.jsx
+++ Projects/paytm-app/frontend/src/pages/Signup.jsx
@@ -1,24 +1,60 @@
+import { useState } from "react"
 import { BottomWarning } from "../components/BottomWarning"
 import { Button } from "../components/Button"
 import { Heading } from "../components/Heading"
 import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
+import axios from "axios"
 
 export const Signup = () => {
+    const [firstName,setFirstName]=useState("");
+    const [lastName,setLastName]=useState("");
+    const [username,setUsername]=useState("");
+    const [password,setPassword]=useState("");
+
     return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
         <Heading label={"Sign up"} />
         <SubHeading label={"Enter your infromation to create an account"} />
-        <InputBox placeholder="John" label={"First Name"} />
-        <InputBox placeholder="Doe" label={"Last Name"} />
-        <InputBox placeholder="harkirat@gmail.com" label={"Email"} />
-        <InputBox placeholder="123456" label={"Password"} />
+
+
+        <InputBox onChange={e=>{
+          setFirstName(e.target.value);
+        }}placeholder="John" label={"First Name"} />
+
+        <InputBox onChange={e=>{
+          setLastName(e.target.value)
+        }}placeholder="Doe" label={"Last Name"} />
+
+        <InputBox onChange={e=>{
+          setUsername(e.target.value)
+        }}placeholder="harkirat@gmail.com" label={"Email"} />
+
+        <InputBox onChange={e=>{
+          setPassword(e.target.value)
+        }}placeholder="123456" label={"Password"} />
+
+
         <div className="pt-4">
-          <Button label={"Sign up"} />
+          <Button onClick={async()=>{
+              const response= await axios.post("http://localhost:3000/api/v1/user/signup",{
+                username,firstName,lastName,password   //you dont have to do eg username:username etc as they are same name
+              });
+              localStorage.setItem("token",response.data.token) //here "token" is just a key, you can give any name for it
+          }} label={"Sign up"} />
         </div>
+
+
         <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
       </div>
     </div>
   </div>
 }
+
+/*i will also install axios in the frontend 
+After signup, when i get back a token from the server, i am going to store it in local storage as i did above
+In browser, go to application and then local storage
+
+Usually in sites,when you logout, the token also gets removed by localStorage.removeItem("token")
+*/
