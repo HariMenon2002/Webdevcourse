6e61014c826f61df951ab9d0428d57735590075e
HariMenon2002 <ultronhari7@gmail.com>
2024-08-03 22:21:04 +0530

    more about cloudflare

--- cloudflare/my-app/Beginning.md
+++ cloudflare/my-app/Beginning.md
@@ -3,3 +3,12 @@ Cloudflare can be used to deploy your backend.Go to https://workers.cloudflare.c
 Create a worker in cloudflare website
 
 Do npm create cloudflare -- my-app. This creates a folder called my-app which will have the modules. Select category as Hello World Example with Hello World worker.Select language as typescript and select no if it asks whether the application is deployed
+
+Do npm run dev here in the terminal
+
+Wrangler is like a command line, so you need to first login to your account by doing the below command in the terminal
+npx wrangler login
+
+Run npx wrangler whoami  to get details about your account
+
+npm run deploy, this will deploy my project, it will also give a l
--- cloudflare/my-app/src/index.ts
+++ cloudflare/my-app/src/index.ts
@@ -11,8 +11,36 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+export default { //cloudflare always comes here, routing should be done from here
+	async fetch(request, env, ctx): Promise<Response> {//async function will return a Promise. Here it is of generic type Response
+		//return new Response('Hello World!');
+	    return Response.json({
+			message:"hi there"
+		})
 	},
 } satisfies ExportedHandler<Env>;
+
+//way to do routing
+// export default {
+// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+// 		console.log(request.body);
+// 		console.log(request.headers);
+// 		console.log(request.method);
+		
+// 		if (request.method === "GET") {
+// 			return Response.json({
+// 				message: "you sent a get request"
+// 			});
+// 		} else {
+// 			return Response.json({
+// 				message: "you did not send a get request"
+// 			});
+// 		}
+// 	},
+// };
+
+
+//Problem with the above method of routing is that its too ugly and we need a library like express to do routing
+//Express cant be used here as it heavily relies on Nodejs dependencies which CloudFlare doesnt have
+
+//We can use Hono
