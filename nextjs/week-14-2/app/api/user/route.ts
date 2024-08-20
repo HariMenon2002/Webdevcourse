8df8ad4489b848a27bff213ebaccc027e0aef9ce
HariMenon2002 <ultronhari7@gmail.com>
2024-08-20 17:44:17 +0530

    connecting database with nextjs

--- nextjs/week-14-2/app/api/user/route.ts
+++ nextjs/week-14-2/app/api/user/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest } from "next/server";
 
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
 
 export async function GET() {  //we can access it in localhost:3000/api/user    
     return Response.json({ username: "harkirat", email: "harkirat@gmail.com" })
@@ -8,9 +10,25 @@ export async function GET() {  //we can access it in localhost:3000/api/user
 //ie if we want a get handler, we have to export a function called GET
 //this is equivalent to app.get("/api/user",(req,res)=>{res.json({username...})})
 
+/*
 export async function POST(req:NextRequest){
 
     const body=await req.json();    
+    console.log(body);
+
+    return Response.json({ message:"you are logged in"})
+}
+*/
+
+export async function POST(req:NextRequest){
+
+    const body=await req.json();
+    await client.user.create({
+        data:{
+            username:body.username,
+            password:body.password
+        }
+    })    
     console.log(body);
 
     return Response.json({ message:"you are logged in"})
